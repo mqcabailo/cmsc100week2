e7f7ed86d41a4782be19884fc023a518abe9fa0c
mqcabailo <matthewqcabailo@gmail.com>
2015-08-26 12:24:19 +0800

    cmsc100ef3l

--- config/router.js
+++ config/router.js
@@ -4,7 +4,13 @@ var teacher = require('./../controllers/teacher');
 module.exports = function(router){
 
       router.route('/students')
-      .get(student.find);
+      .get(student.find)
+      .post(student.insert);
+      
+      router.route('/students/:id')
+      .get(student.findOne)
+      .put(student.update)
+      .delete(student.remove);
       
       router.route('/teacher')
       .get(teacher.find)
